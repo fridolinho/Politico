24962ae0c3d041bba8e894063e8f2f17c0cb3fd9
fridolinho <fridolinho@gmail.com>
2019-02-03 13:57:42 +0200

    [ft-163681807] Admin can edit a specific political party

--- views/party.js
+++ views/party.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/', Party.create);
 router.get('/', Party.getAll);
 router.get('/:id', Party.getOne);
+router.patch('/:id', Party.update);
 
 export default router;
