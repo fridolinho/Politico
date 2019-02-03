cc91171be34ea337295a9bab90edb7ecd9b12a01
= <=>
2019-02-03 16:42:52 +0200

    [ft-163682294] Admin can add political office

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import party from './views/party';
+import office from './views/office';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/v1/parties', party);
+app.use('/api/v1/offices', office);
 
 app.get('/', (req, res) => {
 	res.send('Welcome to Politico app');
