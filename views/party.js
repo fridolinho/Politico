1d7d091c73923bbbb9bda991dd9aee9a12477d96
= <=>
2019-02-04 12:14:10 +0200

    fixed a bug that make test fails

--- views/party.js
+++ views/party.js
@@ -4,7 +4,7 @@ import Party from '../controllers/party';
 const router = express.Router();
 
 router.post('/', Party.createParty);
-router.get('/', Party.getAllPrties);
+router.get('/', Party.getAllParties);
 router.get('/:id', Party.getOneParty);
 router.patch('/:id', Party.update);
 router.delete('/:id', Party.remove);
